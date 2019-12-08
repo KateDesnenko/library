7a61384bd7e884e16352cd7c60bec673e56149cc
kate <katarinadesnenko@gmail.com>
2019-12-08 23:44:54 +0200

    Create page Group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :groups, only: [:show]
   resources :authors, only: [:show]
   resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
