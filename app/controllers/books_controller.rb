b0e959666cae4d479a1a485be76c20db68f443ae
kate <katarinadesnenko@gmail.com>
2019-12-11 19:19:08 +0200

    Add search function for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,10 @@
 class BooksController < ApplicationController
   def index
     # includes для оптимизации запросов к БД (проблема N+1)
-    @books = Book.includes(:authors, :group).all
+    @books = Book.includes(:authors, :group)
+    # помни про SQL injection (экранируй пользовательский ввод)
+    # present-рельсовый метод(Active Cupport) проверяет на присутсвие ввода, противоположный ему blank
+    @books = @books.where('LOWER(title) LIKE ?', "%#{params[:search_name]}%") if params[:search_name].present?
   end
 
   def show
